e2a91b3400eb8d46dfa5fb6e1d6556a4b227537b
Muhammad Aditia Firmansyah <aditia.pikarin@gmail.com>
2022-02-03 13:36:40 +0700

    storing history of moves

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
+import { useState } from 'react';
+import calculateWinner from './calculateWinner';
 import GameBoard from './GameBoard'
 
+interface History {
+  squares: SquareContent[];
+}
+
 const App: React.FC = () => {
+  const [history, setHistory] = useState<History[]>([
+    {
+      squares: Array(9).fill(null),
+    },
+  ])
+
+  const [xIsNext, setXIsNext] = useState<boolean>(true)
+
+  const current: History = history[history.length - 1]
+
+  const handleClick = (i: number): void => {
+    const slicedSquares: SquareContent[] = current.squares.slice()
+
+    if (calculateWinner(slicedSquares) || slicedSquares[i]) {
+      return
+    }
+
+    slicedSquares[i] = xIsNext ? 'X' : 'O'
+    setHistory(history.concat([{ squares: slicedSquares }]))
+    setXIsNext(!xIsNext)
+  }
+
+  const winner: string | null = calculateWinner(current.squares)
+  let status: string
+
+  if (winner) {
+    status = `Winner: ${winner}`
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`
+  }
+
   return (
     <main className="grid min-h-screen place-content-center">
       <section className="game-board">
-        <GameBoard />
+        <GameBoard
+          squares={current.squares}
+          onClick={(i: number): void => handleClick(i)}
+        />
       </section>
-      <section className="ml-5">{/* Game Info */}</section>
+      <section className="mt-4">
+        <div>{status}</div>
+        {/* Game Info */}
+      </section>
     </main>
   )
 }
--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,44 +1,20 @@
-import { useState } from 'react'
-import calculateWinner from './calculateWinner'
 import GameBoardSquare from './GameBoardSquare'
 
-const GameBoard: React.FC = () => {
-  const [squares, setSquares] = useState<SquareContent[]>(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState<boolean>(true)
+interface Props {
+  squares: SquareContent[]
+  onClick: (i: number) => void
+}
 
-  const handleClick = (i: number): void => {
-    const slicedSquares: SquareContent[] = squares.slice()
-
-    if (calculateWinner(slicedSquares) || slicedSquares[i]) {
-      return
-    }
-
-    slicedSquares[i] = xIsNext ? 'X' : 'O'
-    setSquares(slicedSquares)
-    setXIsNext(!xIsNext)
-  }
-
+const GameBoard: React.FC<Props> = ({ squares, onClick }) => {
   const renderSquare = (i: number): JSX.Element => {
-    return <GameBoardSquare value={squares[i]} onClick={() => handleClick(i)} />
-  }
-
-  const winner: string | null = calculateWinner(squares)
-  let status: string
-
-  if (winner) {
-    status = `Winner: ${winner}`
-  } else {
-    status = `Next player: ${xIsNext ? 'X' : 'O'}`
+    return <GameBoardSquare value={squares[i]} onClick={() => onClick(i)} />
   }
 
   return (
-    <div>
-      <div className="mb-3 text-center">{status}</div>
-      <div className="grid grid-cols-3 gap-px border border-gray-500 bg-gray-500">
-        {renderSquare(0)} {renderSquare(1)} {renderSquare(2)}
-        {renderSquare(3)} {renderSquare(4)} {renderSquare(5)}
-        {renderSquare(6)} {renderSquare(7)} {renderSquare(8)}
-      </div>
+    <div className="grid w-max h-max grid-cols-3 gap-px border border-gray-500 bg-gray-500">
+      {renderSquare(0)} {renderSquare(1)} {renderSquare(2)}
+      {renderSquare(3)} {renderSquare(4)} {renderSquare(5)}
+      {renderSquare(6)} {renderSquare(7)} {renderSquare(8)}
     </div>
   )
 }
